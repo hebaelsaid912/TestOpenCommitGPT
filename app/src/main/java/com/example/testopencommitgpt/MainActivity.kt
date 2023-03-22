cb4ef9e180375e3dddacc8006e68fe110afac7ce
hebaelsaid <heba.elsaid@4sale.tech>
2023-03-22 12:07:03 +0200

    this is the commit message

--- app/src/main/java/com/example/testopencommitgpt/MainActivity.kt
+++ app/src/main/java/com/example/testopencommitgpt/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.testopencommitgpt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val clickMeBtn = findViewById<Button>(R.id.clickme)
+        val text = findViewById<TextView>(R.id.textView)
+        var count:Int = 0
+        clickMeBtn.setOnClickListener{
+            count++
+            text.text = count.toString()
+        }
     }
 }
